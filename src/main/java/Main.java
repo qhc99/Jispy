c239d15a56f61c7434d7b5be160e87c8fd89ecf6
nathan <1136026744@qq.com>
2021-02-18 16:15:43 +0800

    modify pom, remove redundant code in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,10 @@
 import org.nathan.interpreter.Jispy;
 
 import java.io.*;
-import java.util.*;
-
-
 
 class Main {
     public static void main(String[] args) {
         JispyApp(args);
-//        benchmark();
     }
 
     static void JispyApp(String[] args) {
@@ -22,22 +18,5 @@ class Main {
         else {
             Jispy.repl();
         }
-    }
-
-    static void benchmark(){
-        List<Integer> l  = new ArrayList<>();
-        for(int i = 0; i < 10_000_000; i++){
-            l.add(1000);
-        }
-        long res = 0, time=0;
-        long t1,t2;
-        for(int i = 0; i < 10_000_000-1;i++){
-            t1 = System.nanoTime();
-            res += l.get(i);
-            t2 = System.nanoTime();
-            time += (t2-t1);
-        }
-        System.out.printf("%fms\n", time/Math.pow(10,6));
-        System.out.println(res);
     }
 }
